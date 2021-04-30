2392a9102c192823ba9230e972c4cc02b4f46066
bribias <brianna.bias@gmail.com>
2021-04-30 08:54:32 -0700

    updated some photos

--- app.js
+++ app.js
@@ -1,5 +1,31 @@
 // import functions and grab DOM elements
+import {
+    getHappinessImage,
+    getStrengthImage,
+} from './utils.js';
+
+const happinessInput = document.getElementByI('happiness-input');
+
+const strengthInput = document.getElementById('strength-input');
+
+const button = document.getElementById('create-button');
+const getHappinessImage = document.getElementById('happiness-image');
+const getStrengthImage = document.getElementById('strength-image');
+
+
 
 // initialize state
+button.addEventListener('click', () => {
+    //get all the values of the inputs
+    const happinessLevel = happinessInput.value;
+    const strengthLevel = strengthInput.value;
 
+const happinessImageUrl = getHappinessImage(happinessLevel);
+const strengthImageUrl = getStrengthImage(strengthLevel);
+
+console.log(happinessUrl, strengthUrl)
+
+getHappinessImage.src = happinessImageUrl;
+    strengthImageUrl.src = strengthImageUrl;
+})
 // set event listeners to update state and DOM
--- test/example.test.js
+++ test/example.test.js
@@ -1,16 +1,87 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
+import { getHappinessImage, getStrengthImage } from '../utils.js'
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('should take in a low number and return the sad image string', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
-    
+    const expected = './assets/sad.png';
+
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    const actual = getHappinessImage(2);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('should take in a middle number and return the neutral image string', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = './assets/neutral.png';
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = getHappinessImage(5);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('should take in a high number and return the happy image string', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = './assets/happy.png';
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = getHappinessImage(9);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('should take in a low number and return the tired image string', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = './assets/tired.png';
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = getStrengthImage(2);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('should take in a middle number and return the normal image string', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = './assets/normal.png';
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = getStrengthImage(5);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('should take in a high number and return the strong image string', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = './assets/strong.png';
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = getStrengthImage(9);
 
     //Expect
     // Make assertions about what is expected versus the actual result
